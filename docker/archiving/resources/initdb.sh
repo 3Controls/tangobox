f78aae6b49aa9814df98d07289be39aaf95972cc
stanisalw47 <stanislaw.cabala@gmail.com>
2017-09-28 15:20:33 +0200

    #15 fixed directories issues

--- docker/archiving/resources/initdb.sh
+++ docker/archiving/resources/initdb.sh
@@ -2,12 +2,10 @@
 
 echo "Adding databases to MariaDB"
 echo "Adding ALARMDB..."
-mysql -u root < /tmp/mambo/db/create_ALARMDB-Mysql.sql
+mysql -u root < /usr/local/db/create_ALARMDB-Mysql.sql
 echo "Adding HDB..."
-mysql -u root < /tmp/mambo/db/create-HDB-MyISAM.sql
+mysql -u root < /usr/local/db/create-HDB-MyISAM.sql
 echo "Adding SNAPDB..."
-mysql -u root < /tmp/mambo/db/create-SNAPDB-InnoDB.sql
+mysql -u root < /usr/local/db/create-SNAPDB-InnoDB.sql
 echo "Adding TDB..."
-mysql -u root < /tmp/mambo/db/create-TDB-MyISAM.sql
-
-
+mysql -u root < /usr/local/db/create-TDB-MyISAM.sql
