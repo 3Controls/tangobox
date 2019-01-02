2cdb89f9cf617b1d6a72d29d88705c32fcc42f8a
Piotr Goryl <piotr.goryl@s2innovation.com>
2019-01-02 13:07:40 +0100

    #17, #23 - adding scripts to load devices configuration from json files

---
+++ scripts/configure-tango.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+json2tango -w -s 1 -d -v ~/work/tangobox/db/archiving.json
+json2tango -w -s 1 -d -v ~/work/tangobox/db/linac.json
+json2tango -w -s 1 -d -v ~/work/tangobox/db/sr.json
+json2tango -w -s 1 -d -v ~/work/tangobox/db/modbus.json
+json2tango -w -s 1 -d -v ~/work/tangobox/db/sardana.json
