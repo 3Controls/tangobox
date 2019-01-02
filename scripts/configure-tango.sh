b271855b65eed093d933ee246af6cec72bbb1e99
Piotr Goryl <piotr.goryl@s2innovation.com>
2019-01-02 15:49:16 +0100

    #25 - adding json load for starters

--- scripts/configure-tango.sh
+++ scripts/configure-tango.sh
@@ -5,3 +5,4 @@ json2tango -w -s 1 -d -v ~/work/tangobox/db/linac.json
 json2tango -w -s 1 -d -v ~/work/tangobox/db/sr.json
 json2tango -w -s 1 -d -v ~/work/tangobox/db/modbus.json
 json2tango -w -s 1 -d -v ~/work/tangobox/db/sardana.json
+json2tango -w -s 1 -d -v ~/work/tangobox/db/starters.json
