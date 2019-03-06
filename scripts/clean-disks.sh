7ef3023f5d2c0921314c0f8d90fb8979103af5d8
Piotr Goryl <piotr.goryl@s2innovation.com>
2019-03-06 17:32:21 +0100

    cleaning script updated with pruning the docker

--- scripts/clean-disks.sh
+++ scripts/clean-disks.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
-
+echo "Before cleaning:"
+df -h | grep /dev/sda1
 sudo apt-get -y clean
 sudo apt-get -y autoclean
-sudo apt-get -y autoremove
+sudo apt-get -y autoremove --purge
 rm -rf /home/tango-cs/.cache/*
-rm -rf /home/tango-cs/work/tangobox/build/*
+sudo rm -rf /home/tango-cs/work/tangobox/build/*
+docker image prune -a
+
+echo "After cleaning:"
+df -h | grep /dev/sda1
